beb25b986168f29f264f4189fa3c50c52704aede
HornyFerret <ombro.de.morto@gmail.com>
2021-07-06 21:16:04 +0300

    Добавьте файлы проекта.

---
+++ practic-7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace practic_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            MakeNegative(number);
+
+        }
+        public static int MakeNegative(int number)
+        {
+            
+                if (number < 0)
+                {
+                    return number;
+                }
+                else if (number == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    int u = number * (-1);
+                    return u;
+                }
+        }
+    }
+}
